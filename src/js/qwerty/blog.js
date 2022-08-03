2adc7160b66653b957e9e42dd0dbfc1cdbe72fa8
Vitaliy <vitaliy060282@gmail.com>
2022-08-03 22:58:21 +0400

    change menu, add mobile version, add scripts, add content-sidebar

--- src/js/qwerty/blog.js
+++ src/js/qwerty/blog.js
@@ -11,3 +11,21 @@
 //     window.location.href = `{$addpath}/search/${searchstr}`;
 //   }
 // }
+
+jQuery(document).ready(function($){
+	$('.dropdown').on('click', function(){
+		$('.dropdown').not($(this)).removeClass('open');
+		$(this).toggleClass('open');
+	});
+
+	$(document).mouseup( function(e){ // событие клика по веб-документу
+		var div = $( ".dropdown-menu" ); // тут указываем ID элемента
+		if ( !div.is(e.target) // если клик был не по нашему блоку
+		    && div.has(e.target).length === 0 ) { // и не по его дочерним элементам
+			div.closest('.dropdown').removeClass('open'); // скрываем его
+		}
+	});
+
+
+
+});
