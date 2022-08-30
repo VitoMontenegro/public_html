4b90caa36e876fe6cd07a57e040bc3de6d158313
Vitaliy <vitaliy060282@gmail.com>
2022-08-30 23:34:53 +0400

    правка скрипта ~fb плеера

--- src/js/qwerty/slider_popup.js
+++ src/js/qwerty/slider_popup.js
@@ -1,15 +1,34 @@
 jQuery(document).ready(function($){
-	var slideIndex = 1;
+
 	var sid = 0;
 	var id = 0;
+	// Индекс слайда слайд
+	var slideIndex = 1;
+	// длина шкалы
 	var width = 1;
-	var	totalImages = $('.pop-slider__toggle:visible').attr('data-items');
+	//время проигрывания
+	var slideDuration = 5;
+	// количество изображений
+	var	totalImages = $('.pop-slider__toggle').attr('data-items');
+	//шкала прогресса
 	var	progressBar = $(".pop-slider__progress-bar");
+	// пауза
+	var mouseOnContainer = false;
+
+
+	$('body').on('mousedown', '.pop-slider__items__content', function() {
+		mouseOnContainer = true;
+	});
+	$('body').on('mouseup', '.pop-slider__items__content', function() {
+		mouseOnContainer = false;
+	});
 
 	showProgress = function() {
+		progressBar.eq( slideIndex-1 ).css('width', 0);
 
-		id = setInterval(progress, (5 / 100) * 1000);
+		console.log('slideIndex: ' + slideIndex);
 
+		id = setInterval(progress, (slideDuration / 100) * 1000);
 		console.log('id:' + id);
 
 		function progress() {
@@ -17,41 +36,26 @@ jQuery(document).ready(function($){
 				clearInterval(id);
 				clearInterval(sid);
 			}
-			
-			if (width >= 100 && slideIndex >= totalImages) {
-				clearInterval(id);
-			} else {
-				width++;
-				progressBar.eq( slideIndex-1 ).css('width', width + '%');
+			if (mouseOnContainer == false) {
+				if (width >= 100 && slideIndex >= totalImages) {
+					clearInterval(id);
+				} else {
+					width++;
+					progressBar.eq( slideIndex-1 ).css('width', width + '%');
+				}
 			}
-			
 		}
 	};
-
-	if (slideIndex <= totalImages) {
-		showProgress();
-	}
-
-	sid = setInterval(function() {
-		if (width >= 100) {
-			if (slideIndex == totalImages) {
-				clearInterval(sid);
-			} else {
-				plusSlides(1);
-			}
-		}
-	}, 100);
 
 	function plusSlides(n) {
 		clearInterval(id);
 		width = 0;
 		showSlides(slideIndex += n);
-		showProgress();
 	}
 
 	function showSlides(n) {
 		var i;
-		var slides = $(".pop-slider__toggle:visible .pop-slider__items li");
+		var slides = $(".pop-slider__toggle .pop-slider__items li");
 
 		if (n > slides.length) {
 			//in case viewing last slide then keep it on that slide.
@@ -66,31 +70,80 @@ jQuery(document).ready(function($){
 
 		slides[slideIndex - 1].style.display = "block";
 
-		//set time
 		if (slideIndex <= totalImages) {
 			showProgress();
 		}
 	}
 
-	$('.story-slider .slick-slide a').on('click', function(){
-		showProgress();
-	});	
 
+	$('body').on('click', '.is-close.is-top', function() {
+		width = 0;
+		slideIndex = 1;
+		mouseOnContainer = false;
 
+		clearInterval(id);
+		clearInterval(sid);
+	});
 
 	$('.story-slidenav-prev').on('click', function() {
-		showProgress();
+		progressBar.eq( slideIndex-1 ).css('width', 0);
+		clearInterval(sid);
 		plusSlides(-1);
-		clearInterval(id);
-		clearInterval(sid);
+		sid = setInterval(function() {
+			if (mouseOnContainer == false && width >= 100) {
+				plusSlides(1);
+				if (slideIndex == totalImages) {
+					clearInterval(sid);
+				}
+			}
+		}, 100);
 	});
-	$('.story-slidenav-next').on('click', function() {
-		showProgress();
-		plusSlides(1);
-		clearInterval(id);
-		clearInterval(sid);
-	});
-	
 
+	$('.story-slidenav-next').on('click', function() {
+		progressBar.eq( slideIndex-1 ).css('width', "100%");
+		clearInterval(sid);
+		plusSlides(1);
+		sid = setInterval(function() {
+			if (mouseOnContainer == false && width >= 100) {
+				plusSlides(1);
+				if (slideIndex == totalImages) {
+					clearInterval(sid);
+				}
+			}
+		}, 100);
+	});
+
+	$('.story-slider .slick-slide a').on('click', function(){
+		var sid = 0;
+		var id = 0;
+
+
+		// Индекс слайда слайд
+		var slideIndex = 1;
+
+		// длина шкалы
+		var width = 1;
+
+		// количество изображений
+		var	totalImages = $('.pop-slider__toggle:visible').attr('data-items'); 
+
+		//шкала прогресса
+		var	progressBar = $(".pop-slider__progress-bar");
+
+		showSlides(slideIndex);
+	});	
+
+	sid = setInterval(function() {
+		if (mouseOnContainer == false && width >= 100) {
+			if (slideIndex == totalImages) {
+				clearInterval(sid);
+			} else {
+				plusSlides(1);
+			}
+		}
+	}, 100);
 
 });
+
+
+
